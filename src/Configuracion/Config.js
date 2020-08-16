d1d0778d98d399850e35d0cd2764b8c76b995140
anali29g <anali.galindos29@gmail.com>
2020-08-15 19:36:57 -0500

    cambio direccion back

--- src/Configuracion/Config.js
+++ src/Configuracion/Config.js
@@ -1,4 +1,4 @@
 
-   const CONFIG= 'https://sigapdev2-consultarecibos-back.herokuapp.com/'   
+   const CONFIG= 'https://gryffindor-back.herokuapp.com/'   
    //const CONFIG= 'http://localhost:8080/';
 export default CONFIG;
